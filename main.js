56087e6c03ae7d357c3e6dd1d0ff786595cee878
yasin-ai <73063465+yasin-ai@users.noreply.github.com>
2021-02-07 17:46:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-//links
-//http://eloquentjavascript.net/09_regexp.html
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
 
 var messages = [], //array that hold the record of each string in chat
@@ -8,16 +5,7 @@ var messages = [], //array that hold the record of each string in chat
   botMessage = "", //var keeps track of what the chatbot is going to say
   botName = 'Dani', //name of the chatbot
   talking = true; //when false the speach function doesn't work
-//
-//
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//edit this function to change what the chatbot says
+
 
 function chatbotResponse() {
   talking = true;
@@ -72,38 +60,25 @@ function chatbotResponse() {
     botMessage = "NO i hate singing and dancing..";
   }
 }
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//****************************************************************
-//
-//
-//
-//this runs each time enter is pressed.
-//It controls the overall input and output
 
 function newEntry() {
-  //if the message from the user isn't empty then run 
+ 
   if (document.getElementById("chatbox").value != "") {
-    //pulls the value from the chatbox ands sets it to lastUserMessage
+   
     lastUserMessage = document.getElementById("chatbox").value;
     var mss = "<b>" + "You" + ":</b> " + " " + lastUserMessage;
-    //sets the chat box to be clear
+    
     document.getElementById("chatbox").value = "";
-    //adds the value of the chatbox to the array messages
+    
     messages.push(mss);
-    //Speech(lastUserMessage);  //says what the user typed outloud
-    //sets the variable botMessage in response to lastUserMessage
+    
     chatbotResponse();
     
-    //add the chatbot's name and message to the array messages
+    
     messages.push("<b>" + botName + ":</b> " + botMessage);
-    // says the message using the text to speech function written below
+    
     Speech(botMessage);
-    //outputs the last few array elements of messages to html
+    
     for (var i = 1; i < 18; i++) {
       if (messages[messages.length - i])
         document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
@@ -111,25 +86,19 @@ function newEntry() {
   }
 }
 
-//text to Speech
-//https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
+
 function Speech(say) {
   if ('speechSynthesis' in window && talking) {
     var utterance = new SpeechSynthesisUtterance(say);
-    //msg.voice = voices[10]; // Note: some voices don't support altering params
-    //msg.voiceURI = 'native';
-    //utterance.volume = 1; // 0 to 1
-    //utterance.rate = 0.1; // 0.1 to 10
-    //utterance.pitch = 1; //0 to 2
-    //utterance.text = 'Hello World';
+    
     utterance.lang = 'en-US';
     speechSynthesis.speak(utterance);
   }
 }
 
-//runs the keypress() function when a key is pressed
+
 document.onkeypress = keyPress;
-//if the key pressed is 'enter' runs the function newEntry()
+
 function keyPress(e) {
   var x = e || window.event;
   var key = (x.keyCode || x.which);
@@ -139,12 +108,11 @@ function keyPress(e) {
   }
   if (key == 38) {
     console.log('hi')
-      //document.getElementById("chatbox").value = lastUserMessage;
+      
   }
 }
 
-//clears the placeholder text ion the chatbox
-//this function is set to run when the users brings focus to the chatbox, by clicking on it
+
 function placeHolder() {
   document.getElementById("chatbox").placeholder = "";
 }
